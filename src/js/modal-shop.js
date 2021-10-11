0f80ae6602377ec361324372b35e77a46bf67c42
ArdeeWest <teleliterator@gmail.com>
2021-10-12 00:28:32 +0300

    Update script modal-shop.js

--- src/js/modal-shop.js
+++ src/js/modal-shop.js
@@ -1,14 +1,21 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-shop-open]'),
-    closeModalBtn: document.querySelector('[data-modal-shop-close]'),
-    modal: document.querySelector('[data-modal-shop]'),
-  };
+  document.addEventListener('DOMContentLoaded', function () {
+    const refs = {
+      openModalBtnList: document.querySelectorAll('[data-modal-shop-open]'),
+      closeModalBtn: document.querySelector('[data-modal-shop-close]'),
+      modal: document.querySelector('[data-modal-shop]'),
+    };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+    for (i = 0; i < refs.openModalBtnList.length; i++) {
+      refs.openModalBtnList[i].addEventListener('click', function () {
+        document.querySelector('[data-modal-shop]').classList.toggle('is-hidden');
+      });
+    }
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+  });
 })();
